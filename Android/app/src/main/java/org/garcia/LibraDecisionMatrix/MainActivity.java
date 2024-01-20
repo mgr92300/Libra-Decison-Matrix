f1c07ef3882b8b070f723a742f5a19e5d553b22b
mgr92300 <99244586+mgr92300@users.noreply.github.com>
2024-01-20 00:02:15 -0700

    Add files via upload

---
+++ Android/app/src/main/java/org/garcia/LibraDecisionMatrix/MainActivity.java
@@ -0,0 +1,30 @@
+package org.garcia.LibraDecisionMatrix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* Button examples to create
+        Button SumButton = (Button) findViewById(R.id.SumButton);
+        SumButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText FirstNumEditText = (EditText) findViewById(R.id.FirstNumEditText);
+                EditText SecondNumEditText = (EditText) findViewById(R.id.SecondNumEditText);
+                TextView ResultsTextView = (TextView) findViewById(R.id.ResulsTextView);
+
+         */
+    }
+}
